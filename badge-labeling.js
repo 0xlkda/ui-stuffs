33737ff117bfaef10315f025a30cbe420ef3ca96
0xlkda <lekhacduyanh@gmail.com>
2023-08-13 15:38:07 +0700

    cleanup badge-labeling

--- badge-labeling.js
+++ badge-labeling.js
@@ -1,46 +1,35 @@
-function randomRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function randomFromZeroTo(value) {
-  return randomRange(0, value)
-}
-
 function insertCss(rule) {
   document.styleSheets.item(0).insertRule(rule)
 }
 
-let initialized = false
 let BADGE_CLASS = 'badge'
 let BADGE_SELECTOR = `.${BADGE_CLASS}`
 
+let unset = "unset"
+let defaultOpts = { left: unset, top: unset, right: unset, bottom: unset }
+
+init()
 function init() {
   insertCss(`
-    ${BADGE_SELECTOR} {
-      position: relative;
-    }
-  `)
+  ${BADGE_SELECTOR} {
+    position: relative;
+  }
+`)
 
   insertCss(`
-    ${BADGE_SELECTOR} .label {
-      content: attr(data-content);
-      position: absolute;
-    }
-  `)
-
-  initialized = true
+  ${BADGE_SELECTOR} .label {
+    content: attr(data-content);
+    position: absolute;
+  }
+`)
 }
 
-function labeling(element, label, { left, top, right, bottom }) {
-  if (!initialized) init()
-
-  element.setAttribute("data-content", label)
+function labeling(element, label, { left, top, right, bottom } = defaultOpts) {
   element.classList.add(BADGE_CLASS)
 
   if (element.label) {
     element.label.textContent = label
   } else {
-    let unset = "unset"
     let labelElement = document.createElement('span')
     labelElement.classList.add("label")
     labelElement.textContent = label
@@ -54,47 +43,6 @@ function labeling(element, label, { left, top, right, bottom }) {
   }
 }
 
-const sample = () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    let labels = ["50% OFF", "Best Sellings", "20% OFF"]
-    let randomLabel = () => labels[randomFromZeroTo(labels.length - 1)]
-
-    document
-      .querySelectorAll(".element")
-      .forEach(badge => {
-        labeling(badge, randomLabel(), { left: 0, top: "-50%" })
-      })
-  })
-
-  return `
-    <style>
-      .container {
-        display: flex;
-        justify-content: space-between;
-        gap: 1rem;
-      }
-
-      .element {
-        width: 100px;
-        height: 56px;
-        border: 1px solid black;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    </style>
-
-    <div class="container">
-      <div class="element">12 x 12</div>
-      <div class="element">24 x 36</div>
-      <div class="element">48 x 32</div>
-    </div>
-  `
-}
-
 export {
-  sample,
   labeling
 }
-
-
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { sample as badges } from './badge-labeling'
+import { sample as badges } from './badge-labeling.sample.js'
 
 document.querySelector('#app').innerHTML = `
   ${badges()}
