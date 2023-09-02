6c2c087c4d80230188380071497d8c0698c1bce0
0xlkda <lekhacduyanh@gmail.com>
2023-09-02 17:58:10 +0700

    added UI for calculate time to ground

--- free-fall.js
+++ free-fall.js
@@ -1,26 +1,47 @@
 // Define constants
 const g = 9.81 // Acceleration due to gravity in m/s²
-const initialHeight = 1000 // Initial height in meters
 const initialVelocity = 0 // Initial velocity in m/s
-const timeStep = 0.01 // Time step in seconds
-const totalTime = 60 // Total simulation time in seconds
 
-// 
-let height = initialHeight
-let velocity = initialVelocity
-let time = 0
+// Time step in seconds
+const timeStepInput = document.querySelector('input[name="timestep"]')
+const heightInput = document.querySelector('input[name="height"]')
 
-// Simulate the motion
-while (height > 0) {
-  // Calculate the new height and velocity using equations of motion
-  const newHeight = height - (velocity * timeStep) - (0.5 * g * timeStep * timeStep)
-  const newVelocity = velocity + g * timeStep
+let timeStep = parseFloat(timeStepInput.value)
+let height = parseFloat(heightInput.value)
 
-  // Update variables
-  height = newHeight < 0 ? 0: newHeight
-  velocity = newVelocity
-  time += timeStep
-
-  // Output the current state of the object
-  console.log(`Time: ${time.toFixed(2)}s, Height: ${height.toFixed(2)}m, Velocity: ${velocity.toFixed(2)}m/s`)
+function changeTimeStep(e) {
+  timeStep = parseFloat(e.value)
+  calculateTime()
 }
+
+function calculateTime() {
+  height = parseFloat(heightInput.value)
+  timeStep = parseFloat(timeStepInput.value)
+
+  let velocity = initialVelocity
+  let time = 0
+  let steps = []
+
+  // Simulate the motion
+  while (height > 0) {
+    // Calculate the new height and velocity using equations of motion
+    const newHeight = height - (velocity * timeStep) - (0.5 * g * timeStep * timeStep)
+    const newVelocity = velocity + g * timeStep
+
+    // Update variables
+    height = newHeight < 0 ? 0: newHeight
+    velocity = newVelocity
+    time += timeStep
+
+    // Output the current state of the object
+    steps.push(`
+    <li>
+      Time: ${time.toFixed(2)}s, Height: ${height.toFixed(2)}m, Velocity: ${velocity.toFixed(2)}m/s
+    </li>
+    `)
+  }
+
+  document.getElementById('result').innerHTML = steps.join('')
+}
+
+calculateTime(1000)
